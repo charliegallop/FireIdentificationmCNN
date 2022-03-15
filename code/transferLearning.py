84544f8c9372289967b746ac1baf6a86304baccb
charliegallop <c.gallop@protonmail.com>
2022-03-15 11:59:06 +0000

    Finished transfer learning bit

---
+++ code/transferLearning.py
@@ -0,0 +1,330 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 10 14:48:30 2022
+
+@author: charlie
+
+following: https://wwww.tensorflow.org/tutorials/images/transfer_learning
+"""
+#%%
+
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import PIL
+import tensorflow as tf
+
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+
+import pathlib
+#%%
+
+data_dir =pathlib.Path('/home/charlie/Documents/Uni/Exeter - Data Science/MTHM602_Trends_in_data_science_and_AI/Project/data/Training and Validation')
+test_dir =pathlib.Path('/home/charlie/Documents/Uni/Exeter - Data Science/MTHM602_Trends_in_data_science_and_AI/Project/data/Testing')
+
+#%%
+
+image_count = len(list(data_dir.glob('*/*.jpg')))
+print(image_count)
+
+
+#%%
+
+fire = list(data_dir.glob('fire/*'))
+img = PIL.Image.open(str(fire[0]))
+img.show()
+
+#%% Load data into a tf dataset
+
+batch_size = 32
+img_height = 160
+img_width = 160
+
+# Validation split 80/20
+
+train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    data_dir,
+    validation_split = 0.2,
+    subset = "training",
+    shuffle = True,
+    seed = 123,
+    image_size = (img_height, img_width),
+    batch_size = batch_size
+    )
+#%%
+
+val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    data_dir,
+    validation_split = 0.2,
+    subset = "validation",
+    shuffle = True,
+    seed = 123,
+    image_size = (img_height, img_width),
+    batch_size = batch_size    
+    )
+
+#%%
+test_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    test_dir,
+    shuffle = True,
+    seed = 123,
+    image_size = (img_height, img_width),
+    batch_size = batch_size
+    )
+
+#%% Get class names
+
+class_names = train_ds.class_names
+print(class_names)
+
+#%% Visualise Data
+
+plt.figure(figsize = (10, 10))
+for images, labels in train_ds.take(1):
+    for i in range(9):
+        ax = plt.subplot(3, 3, i + 1)
+        plt.imshow(images[i].numpy().astype("uint8"))
+        plt.title(class_names[labels[i]])
+        plt.axis("off")
+
+#%% Configure the dataset for performance
+
+AUTOTUNE = tf.data.AUTOTUNE
+
+train_ds = train_ds.prefetch(buffer_size = AUTOTUNE)
+val_ds = val_ds.prefetch(buffer_size = AUTOTUNE)
+test_ds = test_ds.prefetch(buffer_size = AUTOTUNE)
+
+# %% Rescale pixel values for use in imported model
+
+# MobileNetV2 expects pixel values between [-1, 1] but they are currently [0, 255]
+
+preprocess_input = tf.keras.applications.mobilenet_v2.preprocess_input
+rescale = tf.keras.layers.experimental.preprocessing.Rescaling(1./127.5, offset = -1)
+
+# %% Create the base model from pre-trained convnets
+
+## By specifying "include_top=False" argument you load a network that doesn't include the classification layers at the top, which is ideal for feature extraction
+
+IMG_SHAPE = (img_width, img_height) + (3,)
+base_model = tf.keras.applications.MobileNetV2(input_shape = IMG_SHAPE,
+                                               include_top=False,
+                                               weights='imagenet')
+
+image_batch, label_batch = next(iter(train_ds))
+feature_batch = base_model(image_batch)
+print(feature_batch.shape)
+
+#%% Feature Extraction
+
+base_model.trainable = False
+base_model.summary()
+
+#%% Add classification head
+
+global_average_layer = tf.keras.layers.GlobalAveragePooling2D()
+feature_batch_average = global_average_layer(feature_batch)
+print(feature_batch_average.shape)
+
+#%%
+
+prediction_layer = tf.keras.layers.Dense(1)
+prediction_batch = prediction_layer(feature_batch_average)
+print(prediction_batch.shape)
+
+#%% Chain together the model
+
+inputs = tf.keras.Input(shape = (160, 160, 3))
+x = base_model(inputs, training = False)
+x = global_average_layer(x)
+x = tf.keras.layers.Dropout(0.2)(x)
+outputs = prediction_layer(x)
+model = tf.keras.Model(inputs, outputs)
+
+# %% Compile the model
+
+base_learning_rate = 0.0001
+model.compile(optimizer = tf.keras.optimizers.Adam(learning_rate = base_learning_rate),
+              loss = tf.keras.losses.BinaryCrossentropy(from_logits=(True)),
+              metrics = ['accuracy'])
+#%%
+model.summary()
+# %% Train the model
+
+initial_epochs = 10
+loss0, accuracy0 = model.evaluate(val_ds)
+
+# %%
+print("initial loss: {:.2f}".format(loss0))
+print("initial accuracy: {:.2f}".format(accuracy0))
+
+#%%
+history = model.fit(train_ds,
+                    epochs = initial_epochs,
+                    validation_data = val_ds)
+#%% Learning curves
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+plt.figure(figsize = (8, 8))
+plt.subplot(2, 1, 1)
+plt.plot(acc, label = 'Training Accuracy')
+plt.plot(val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.ylabel('Accuracy')
+plt.ylim([min(plt.ylim()),1])
+plt.title('Training and Validation Accuracy')
+
+
+plt.subplot(2, 1, 2)
+plt.plot(loss, label='Training Loss')
+plt.plot(val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.ylabel('Cross Entropy')
+plt.ylim([0,1.0])
+plt.title('Training and Validation Loss')
+plt.xlabel('epoch')
+plt.show()
+
+#%% Fine tuning
+
+# Previously the base model weights were not updated and only a few layers on top of the MobileNetV2 was trained
+# Unfreeze and train the top layers of the pre-trained model to fine tune to the dataset
+
+## Un-freeze top layers of the model
+
+base_model.trainable = True
+
+#%%
+# Let's take a look to see how many layers are in the base model
+print("Number of layers in the base model: ", len(base_model.layers))
+
+# Fine-tune from this layer onwards
+fine_tune_at = 100
+
+# Freeze all the layers before the `fine_tune_at` layer
+for layer in base_model.layers[:fine_tune_at]:
+  layer.trainable = False
+
+#%% Compile the model
+
+## Important to use a lower training weight since using more layers, otherwise
+# it could overfit very quickly 
+
+model.compile(loss = tf.keras.losses.BinaryCrossentropy(from_logits = True),
+              optimizer = tf.keras.optimizers.RMSprop(learning_rate = base_learning_rate/10),
+              metrics = ['accuracy'])
+
+model.summary()
+
+#%%
+
+print(len(model.trainable_variables))
+
+#%% Continue training the model
+
+fine_tune_epochs = 20
+total_epochs = initial_epochs + fine_tune_epochs
+
+history_fine = model.fit(train_ds,
+                         epochs = total_epochs,
+                         initial_epoch = history.epoch[-1],
+                         validation_data = val_ds)
+
+#%% Learning Curves
+
+acc += history_fine.history['accuracy']
+val_acc += history_fine.history['val_accuracy']
+
+loss += history_fine.history['loss']
+val_loss += history_fine.history['val_loss']
+#%%
+
+plt.figure(figsize=(8, 8))
+plt.subplot(2, 1, 1)
+plt.plot(acc, label='Training Accuracy')
+plt.plot(val_acc, label='Validation Accuracy')
+plt.ylim([0.8, 1])
+plt.plot([initial_epochs-1,initial_epochs-1],
+          plt.ylim(), label='Start Fine Tuning')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(2, 1, 2)
+plt.plot(loss, label='Training Loss')
+plt.plot(val_loss, label='Validation Loss')
+plt.ylim([0, 1.0])
+plt.plot([initial_epochs-1,initial_epochs-1],
+         plt.ylim(), label='Start Fine Tuning')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.xlabel('epoch')
+plt.show()
+
+#%% Verifying on test set
+
+loss, accuracy = model.evaluate(test_ds)
+print('Test accuracy :', accuracy)
+
+# %%
+# Retrieve a batch of images from the test set
+image_batch, label_batch = test_ds.as_numpy_iterator().next()
+predictions = model.predict_on_batch(image_batch).flatten()
+
+# Apply a sigmoid since our model returns logits
+predictions = tf.nn.sigmoid(predictions)
+predictions = tf.where(predictions < 0.5, 0, 1)
+
+print('Predictions:\n', predictions.numpy())
+print('Labels:\n', label_batch)
+
+plt.figure(figsize=(10, 10))
+for i in range(9):
+  ax = plt.subplot(3, 3, i + 1)
+  plt.imshow(image_batch[i].astype("uint8"))
+  plt.title(class_names[predictions[i]])
+  plt.axis("off")
+
+#%%
+
+y_pred = model.predict_classes(test_ds)
+
+#%%
+import pandas as pd
+con_mat = tf.math.confusion_matrix(labels = label_batch, predictions=predictions).numpy()
+con_mat_norm = np.around(con_mat.astype('float')/con_mat.sum(axis = 1)[:, np.newaxis], decimals = 2)
+
+con_mat_df = pd.DataFrame(con_mat_norm,
+                          index = class_names,
+                          columns = class_names)
+
+#%%
+import seaborn as sns
+figure = plt.figure(figsize = (8, 8))
+sns.heatmap(con_mat_df, annot = True, cmap = plt.cm.Blues)
+plt.tight_layout()
+plt.ylabel('True label')
+plt.xlabel('Predicted label')
+plt.show()
+
+#%% ROC Curve
+
+from sklearn.metrics import roc_curve,roc_auc_score
+
+fpr , tpr , thresholds = roc_curve ( label_batch, predictions)
+
+def plot_roc_curve(fpr,tpr): 
+  plt.plot(fpr,tpr) 
+  plt.axis([0,1,0,1]) 
+  plt.xlabel('False Positive Rate') 
+  plt.ylabel('True Positive Rate') 
+  plt.show()    
+  
+plot_roc_curve (fpr,tpr) 
