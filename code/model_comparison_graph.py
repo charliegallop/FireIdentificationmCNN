1d1ac871bdad400be90138c40b19439472c21a7a
charliegallop <c.gallop@protonmail.com>
2022-08-10 13:57:46 +0100

    final commit

--- code/model_comparison_graph.py
+++ code/model_comparison_graph.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
+import altair as alt
 
 #%%
 
@@ -24,8 +25,54 @@ df.rename(columns={'Model':'model',
 'Depth':'depth', 
 'Time (ms) per inference step (CPU)':'time_CPU', 
  'Time (ms) per inference step (GPU)':'time_GPU'}, inplace=True)
+
+#%%
+df.head()
 # %%
 
+graph = alt.Chart(df).mark_point(filled = True,
+        size = 100,
+).encode(
+    alt.X('parameters',
+        scale=alt.Scale(domain=(0, 150)),
+        axis=alt.Axis(title='Number of Parameters (Millions)')
+    ),
+    alt.Y('top1',
+        scale=alt.Scale(domain=(0.68, 0.85)),
+        axis=alt.Axis(format = '%', title='Top 1 Accuracy')
+    ),
+    color = alt.Color('group', legend=alt.Legend(title="Model Family")),
+    size = alt.Size('size', legend=alt.Legend(title="Size in MB", orient = 'right'), scale = alt.Scale(range= [100, 2500]))
+).properties(
+    height = 500,
+    width = 1000
+)
+
+text = alt.Chart(df).mark_text(
+    align='left',
+    baseline='top',
+    dx=8,
+    dy = 5,
+    size = 15
+).encode(
+    x='parameters:Q',
+    y='top1:Q',
+    text=alt.Text('model')
+)
+
+chart = (graph+text).configure_axis(
+    grid=False,
+    labelFontSize=15,
+    titleFontSize=15
+).configure_view(
+    strokeWidth=0
+)
+chart
+#%%
+
+chart.save('../plots/model_comparison.svg')
+
+# %%
 x = df["parameters"]
 y = df["top1"]
 
@@ -50,5 +97,7 @@ df["parameters"]
 
 string = ResNet
 
-df["group"] = df["model"].apply(lambda x: string if x.contains(string, regex = False, string))
+df["group"] = df["model"].apply(lambda x: string if x.contains(string, regex = False))
+# %%
+
 # %%
